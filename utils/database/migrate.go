cd529b8fb18fd191ea33cc6707a333ab6fbc2f9b
Rizqi Ghozali <rizqi.ghoz@gmail.com>
2023-11-13 15:43:19 +0700

    feat(migration): Menambahkan utility untuk database/migration

--- utils/database/migrate.go
+++ utils/database/migrate.go
@@ -4,12 +4,21 @@ import (
 	DataArticleCategory "FinalProject/features/article_categories/data"
 	DataArticle "FinalProject/features/articles/data"
 	DataUser "FinalProject/features/users/data"
+	"FinalProject/utils/database/migration"
 
 	"gorm.io/gorm"
 )
 
 func Migrate(db *gorm.DB) {
-	db.AutoMigrate(DataUser.User{})
-	db.AutoMigrate(DataArticleCategory.ArticleCategory{})
-	db.AutoMigrate(DataArticle.Article{})
+	migrator := migration.NewMySqlMigrator(db)
+	migrator.DropTable([]migration.Table{
+		DataUser.User{},
+		DataArticleCategory.ArticleCategory{},
+		DataArticle.Article{},
+	}...)
+	migrator.CreateTable([]migration.Table{
+		DataUser.User{},
+		DataArticleCategory.ArticleCategory{},
+		DataArticle.Article{},
+	}...)
 }
