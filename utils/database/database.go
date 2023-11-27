e935ed1e3f54d5992b6f3fcc49ee3ff8a00859cb
irvanhau <irvanhau@gmail.com>
2023-11-27 15:22:24 +0700

    Update: Setup Docker Compose

--- utils/database/database.go
+++ utils/database/database.go
@@ -4,19 +4,26 @@ import (
 	"FinalProject/configs"
 	"fmt"
 
-	"github.com/sirupsen/logrus"
+	"github.com/labstack/gommon/log"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-func InitDB(c configs.ProgrammingConfig) *gorm.DB {
-	connStr := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", c.DBUser, c.DBPass, c.DBHost, c.DBPort, c.DBName)
-	// connStr := "root:@tcp(127.0.0.1:3306)/mentalhealth?charset=utf8mb4&parseTime=True&loc=Local"
+func InitDB(c configs.ProgrammingConfig) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		c.DBUser,
+		c.DBPass,
+		c.DBHost,
+		c.DBPort,
+		c.DBName,
+	)
+	log.Print(dsn)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-	db, err := gorm.Open(mysql.Open(connStr), &gorm.Config{})
 	if err != nil {
-		logrus.Fatal("Cannot connect database : ", err.Error())
+		log.Error("terjadi kesalahan pada database, error:", err.Error())
+		return nil, err
 	}
 
-	return db
+	return db, nil
 }
