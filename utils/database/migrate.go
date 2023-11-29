3faeb3b9f6e5af33bf751f8de611bc207ee0ecbf
irvanhau <irvanhau@gmail.com>
2023-11-29 15:33:49 +0700

    Test: Migration

--- utils/database/migrate.go
+++ utils/database/migrate.go
@@ -1,21 +1,21 @@
 package database
 
 import (
-	DataArticleCategory "FinalProject/features/article_categories/data"
-	DataArticle "FinalProject/features/articles/data"
-	DataDoctor "FinalProject/features/doctor/data"
+	// DataArticleCategory "FinalProject/features/article_categories/data"
+	// DataArticle "FinalProject/features/articles/data"
+	// DataDoctor "FinalProject/features/doctor/data"
 	DataPatient "FinalProject/features/patients/data"
-	DataTransaction "FinalProject/features/transaction/data"
-	DataUser "FinalProject/features/users/data"
+	// DataTransaction "FinalProject/features/transaction/data"
+	// DataUser "FinalProject/features/users/data"
 
 	"gorm.io/gorm"
 )
 
 func Migrate(db *gorm.DB) {
-	db.AutoMigrate(DataUser.User{})
-	db.AutoMigrate(DataArticleCategory.ArticleCategory{})
-	db.AutoMigrate(DataArticle.Article{})
-	db.AutoMigrate(DataDoctor.Doctor{})
+	// db.AutoMigrate(DataUser.User{})
+	// db.AutoMigrate(DataArticleCategory.ArticleCategory{})
+	// db.AutoMigrate(DataArticle.Article{})
+	// db.AutoMigrate(DataDoctor.Doctor{})
 	db.AutoMigrate(DataPatient.PatientAccount{})
-	db.AutoMigrate(DataTransaction.Transaction{})
+	// db.AutoMigrate(DataTransaction.Transaction{})
 }
