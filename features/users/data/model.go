7a8ff24518404a591f40d3aacdcbb4986126c68a
irvanhau <irvanhau@gmail.com>
2023-11-29 15:01:50 +0700

    Update: User Model

--- features/users/data/model.go
+++ features/users/data/model.go
@@ -1,20 +1,22 @@
 package data
 
 import (
-	"FinalProject/features/articles/data"
+	dataArticle "FinalProject/features/articles/data"
+	dataDoctor "FinalProject/features/doctor/data"
 
 	"gorm.io/gorm"
 )
 
 type User struct {
 	*gorm.Model
-	Name           string         `gorm:"column:name;type:varchar(255)"`
-	Email          string         `gorm:"column:email;unique;type:varchar(255)"`
-	Password       string         `gorm:"column:password;type:varchar(255)"`
-	Role           string         `gorm:"type:enum('Admin','Patient','Doctor');column:role"`
-	Status         string         `gorm:"type:enum('Active','Inactive','Suspend');column:status"`
-	TokenResetPass string         `gorm:"column:token_reset_pass;type:varchar(255)"`
-	Articles       []data.Article `gorm:"foreignKey:UserID"`
+	Name           string                `gorm:"column:name;type:varchar(255)"`
+	Email          string                `gorm:"column:email;unique;type:varchar(255)"`
+	Password       string                `gorm:"column:password;type:varchar(255)"`
+	Role           string                `gorm:"type:enum('Admin','Patient','Doctor');column:role"`
+	Status         string                `gorm:"type:enum('Active','Inactive','Suspend');column:status"`
+	TokenResetPass string                `gorm:"column:token_reset_pass;type:varchar(255)"`
+	Articles       []dataArticle.Article `gorm:"foreignKey:UserID"`
+	Doctors        []dataDoctor.Doctor   `gorm:"foreignKey:UserID"`
 }
 
 func (User) TableName() string {
