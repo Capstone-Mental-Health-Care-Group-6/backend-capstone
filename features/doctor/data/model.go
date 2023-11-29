16d483b562ab04419f26853ed0a36ac9020c9ee2
Takamanu <fauzanalivijsma@gmail.com>
2023-11-30 05:35:11 +0700

    fix: doctor status request and confirmed

--- features/doctor/data/model.go
+++ features/doctor/data/model.go
@@ -24,7 +24,7 @@ type Doctor struct {
 	DoctorCV                string                    `gorm:"column:doctor_cv;type:varchar(255)"`
 	DoctorIjazah            string                    `gorm:"column:doctor_ijazah;type:varchar(255)"`
 	DoctorBalance           uint                      `gorm:"column:doctor_balance"`
-	DoctorStatus            string                    `gorm:"column:doctor_status;type:enum('active','not_active')"`
+	DoctorStatus            string                    `gorm:"column:doctor_status;type:enum('request','confirmed')"`
 	DoctorExpertiseRelation []DoctorExpertiseRelation `gorm:"foreignKey:DoctorID"`
 	DoctorWorkadays         []DoctorWorkadays         `gorm:"foreignKey:DoctorID"`
 	DoctorRating            []DoctorRating            `gorm:"foreignKey:DoctorID"`
