a3c0609392bf062a1690955a0f46684152643ad9
Takamanu <fauzanalivijsma@gmail.com>
2023-12-16 22:37:04 +0700

    fix: bug transaction and doctor

--- features/transaction/handler/request.go
+++ features/transaction/handler/request.go
@@ -1,23 +1,20 @@
 package handler
 
-import "mime/multipart"
-
 type InputRequest struct {
-	TopicID           uint           `json:"topic_id" form:"topic_id"`
-	PatientID         uint           `json:"patient_id" form:"patient_id"`
-	DoctorID          uint           `json:"doctor_id" form:"doctor_id"`
-	MethodID          uint           `json:"method_id" form:"method_id"`
-	DurationID        uint           `json:"duration_id" form:"duration_id"`
-	CounselingID      uint           `json:"counseling_id" form:"counseling_id"`
-	UserID            uint           `json:"user_id" form:"user_id"`
-	CounselingSession uint           `json:"counseling_session" form:"counseling_session"`
-	CounselingType    string         `json:"counseling_type" form:"counseling_type"`
-	PriceMethod       uint           `json:"price_method" form:"price_method"`
-	PriceDuration     uint           `json:"price_duration" form:"price_duration"`
-	PriceCounseling   uint           `json:"price_counseling" form:"price_counseling"`
-	PaymentProof      multipart.File `json:"payment_proof" form:"payment_proof"`
-	PriceResult       uint           `json:"price_result" form:"price_result"`
-	PaymentType       string         `json:"payment_type" form:"payment_type"`
+	TopicID           uint   `json:"topic_id" form:"topic_id" validate:"required"`
+	PatientID         uint   `json:"patient_id" form:"patient_id" validate:"required"`
+	DoctorID          uint   `json:"doctor_id" form:"doctor_id" validate:"required"`
+	MethodID          uint   `json:"method_id" form:"method_id" validate:"required"`
+	DurationID        uint   `json:"duration_id" form:"duration_id" validate:"required"`
+	CounselingID      uint   `json:"counseling_id" form:"counseling_id" validate:"required"`
+	UserID            uint   `json:"user_id" form:"user_id" validate:"required"`
+	CounselingSession uint   `json:"counseling_session" form:"counseling_session" validate:"required"`
+	CounselingType    string `json:"counseling_type" form:"counseling_type" validate:"required"`
+	PriceMethod       uint   `json:"price_method" form:"price_method" validate:"required"`
+	PriceDuration     uint   `json:"price_duration" form:"price_duration" validate:"required"`
+	PriceCounseling   uint   `json:"price_counseling" form:"price_counseling" validate:"required"`
+	PriceResult       uint   `json:"price_result" form:"price_result" validate:"required"`
+	PaymentType       string `json:"payment_type" form:"payment_type" validate:"required"`
 }
 
 type UpdateRequest struct {
